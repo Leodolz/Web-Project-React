cb4e029b60bab89bc02ced443a73cfda157999bb
Leandro Hurtado <leodolz14@gmail.com>
2020-04-28 16:53:38 -0400

    Refactored Questions accordion to new component

--- WebExamAppFrontend/exam-frontend/src/components/adminUIs/ExamEditor.jsx
+++ WebExamAppFrontend/exam-frontend/src/components/adminUIs/ExamEditor.jsx
@@ -3,6 +3,7 @@ import TextOverlayForm from '../smallComponents/TextOverlayForm';
 import SelectBox from '../smallComponents/SelectBox';
 import QuestionEditor from '../smallComponents/QuestionEditor';
 import Accordion from '../Accordion';
+import QuestionsViewer from '../smallComponents/QuestionsViewer';
 
 
 class ExamEditor extends Component {
@@ -98,38 +99,6 @@ class ExamEditor extends Component {
         exam.RealExamQuestion = newArray;
         this.setState({exam:exam});
     }
-
-    renderOptionList = (question) => 
-    {
-        let list = [];
-        //let closeButton = <button type="button" onClick={this.hideComponent} className="close">x</button>;
-        list.push(<li key={"Options"+question.questionId} className= "questionTitle"><p>Options:</p></li>);
-        for(let i=0;i<question.optionElement.options.length;i++)
-        {
-            let listElement = (
-                <li className="optionElement" title = {question.optionElement.options[i]} key={"O"+i}><p>{(i+1)}. {question.optionElement.options[i]}</p></li>
-            )
-            list.push(listElement);
-        }
-        list = this.renderAnswersList(question, list);
-        return list;
-    }
-    renderAnswersList =(question, list) =>
-    {
-        let answerTag = null;
-        if(question.optionElement.type == "Multiple")
-            answerTag="Answers:";
-        else answerTag="Answer:";
-        list.push(<li className= "questionTitle" key={"AN"+question.questionId}>{answerTag}</li>);
-        for(let i=0;i<question.optionElement.answer.length;i++)
-        {
-            let listElement = (
-                <li className="optionElement" key={"A"+i+""+question.Id}><p>{question.optionElement.answer[i]}</p></li>
-            );
-            list.push(listElement);
-        }
-        return list;
-    }    
     PopulateSubAreaOptions = () =>
     {
         let areasBody = [];
@@ -214,46 +183,13 @@ class ExamEditor extends Component {
         newExam.staticQuestions = event.target.value;
         this.setState({exam:newExam});
     }
-    GetQuestionsBody = (questions) =>
-    {
-        let questionsList = [];
-        for(let i=0;i<questions.length;i++)
-        {
-            let container = 
-            {
-                title: "Question "+(i+1),
-                body: (<>
-                    <p className = "questionTitle">{questions[i].title}</p>
-                    <ul className="myUL">
-                        {this.renderOptionList(questions[i])}
-                    </ul>
-                    </>
-                )
-            }
-            questionsList.push(container);
-        }
-        return [
-            {
-                title:"Questions",
-                body:
-                {
-                    after:(
-                        <React.Fragment>
-                            <button >Manage Questions</button> 
-                        <hr/>
-                        </React.Fragment>),
-                    multi: questionsList
-                },
-            },
-        ];
-    }
+
     render() 
     {  
         console.log(this.state.exam.staticQuestions);
         let overlay = this.GetOverlayedForm();
         let fromDate = this.FormatDate(this.state.exam.fromDate);
         let untilDate = this.FormatDate(this.state.exam.untilDate);
-        let accordions = this.GetQuestionsBody(this.state.exam.RealExamQuestion.slice());
         return (
             <React.Fragment>
                 <div className="">
@@ -270,7 +206,7 @@ class ExamEditor extends Component {
 
                 </div>
                 <QuestionEditor getNewQuestion={this.getNewQuestion}  findItemsInArray={this.findItemsInArray} DeleteComponentInArray={this.DeleteComponentInArray}/>
-                <Accordion accordions={accordions} />
+                <QuestionsViewer questions = {this.state.exam.RealExamQuestion.slice()}/>
                 <br/>
                 <button onClick={this.showActive}>Upload Exam</button>
                 {overlay}
---
+++ WebExamAppFrontend/exam-frontend/src/components/smallComponents/QuestionsViewer.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import Accordion from '../Accordion';
+
+class QuestionsViewer extends Component {
+    state = {  }
+    render() 
+    {
+        let accordions = this.GetQuestionsBody(this.props.questions); 
+        return (
+            <Accordion accordions={accordions} />
+          );
+    }
+
+    GetQuestionsBody = (questions) =>
+    {
+        let questionsList = [];
+        for(let i=0;i<questions.length;i++)
+        {
+            let container = 
+            {
+                title: "Question "+(i+1),
+                body: (<>
+                    <p className = "questionTitle">{questions[i].title}</p>
+                    <ul className="myUL">
+                        {this.renderOptionList(questions[i])}
+                    </ul>
+                    </>
+                )
+            }
+            questionsList.push(container);
+        }
+        return [
+            {
+                title:"Questions",
+                body:
+                {
+                    after:(
+                        <>
+                            <button >Manage Questions</button> 
+                        <hr/>
+                        </>),
+                    multi: questionsList
+                },
+            },
+        ];
+    }
+    renderOptionList = (question) => 
+    {
+        let list = [];
+        //let closeButton = <button type="button" onClick={this.hideComponent} className="close">x</button>;
+        list.push(<li key={"Options"+question.questionId} className= "questionTitle"><p>Options:</p></li>);
+        for(let i=0;i<question.optionElement.options.length;i++)
+        {
+            let listElement = (
+                <li className="optionElement" title = {question.optionElement.options[i]} key={"O"+i}><p>{(i+1)}. {question.optionElement.options[i]}</p></li>
+            )
+            list.push(listElement);
+        }
+        list = this.renderAnswersList(question, list);
+        return list;
+    }
+    renderAnswersList =(question, list) =>
+    {
+        let answerTag = null;
+        if(question.optionElement.type == "Multiple")
+            answerTag="Answers:";
+        else answerTag="Answer:";
+        list.push(<li className= "questionTitle" key={"AN"+question.questionId}>{answerTag}</li>);
+        for(let i=0;i<question.optionElement.answer.length;i++)
+        {
+            let listElement = (
+                <li className="optionElement" key={"A"+i+""+question.Id}><p>{question.optionElement.answer[i]}</p></li>
+            );
+            list.push(listElement);
+        }
+        return list;
+    }    
+}
+ 
+export default QuestionsViewer;
